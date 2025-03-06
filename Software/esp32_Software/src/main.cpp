63c74f0fe4d7c1216e34fed0a0aa8bdb34ad1fc4
SaschaZumstein <153103542+SaschaZumstein@users.noreply.github.com>
2025-03-06 11:28:42 +0100

    Cam test

--- Software/esp32_Software/src/main.cpp
+++ Software/esp32_Software/src/main.cpp
@@ -1,14 +1,12 @@
-#include <Arduino.h>
+#include "Arduino.h"
 
-#define PIN_FLASHLIGHT 4
 
 void setup() {
-  pinMode(PIN_FLASHLIGHT, OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
-  digitalWrite(PIN_FLASHLIGHT, HIGH);
-  delay(1000);
-  digitalWrite(PIN_FLASHLIGHT, LOW);
-  delay(1000);
+  Serial.println("Hallo");
 }
+
+
