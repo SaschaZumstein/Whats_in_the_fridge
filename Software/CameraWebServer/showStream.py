4c6b0bafe2085c3e65369de0ad9d1f6393701340
Sascha Zumstein <zumstein.sascha@bluewin.ch>
2025-03-14 17:20:58 +0100

    cam works

--- Software/CameraWebServer/showStream.py
+++ Software/CameraWebServer/showStream.py
@@ -4,26 +4,26 @@ import requests
 # ESP32-CAM Stream-URL
 URL = "http://192.168.204.60"
 
-# Auflösung auf UXGA (1600x1200) setzen
+# Set resolution to UXGA (1600x1200)
 requests.get(URL + "/control?var=framesize&val=10")
 
-# Video-Stream starten
+# Start video-stream
 cap = cv2.VideoCapture(URL + ":81/stream")
 if not cap.isOpened():
-    print("Fehler: Kann den Stream nicht öffnen!")
+    print("Error: Cannot open the stream!")
     exit()
 
 while True:
     ret, frame = cap.read()
     if not ret:
-        print("Fehler: Kein Frame empfangen!")
+        print("Error: No frame received!")
         break
     
-    # In Graustufen umwandeln
+    # Convert to grayscale
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow("ESP32-CAM Stream (Grayscale)", gray_frame)
     
-    # Beenden mit q
+    # escape with q
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
