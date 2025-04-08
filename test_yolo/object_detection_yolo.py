782c2b02c09d38b13c0901d8122197c8d4a47601
Sascha Zumstein <zumstein.sascha@bluewin.ch>
2025-04-08 18:37:04 +0200

    test

--- test_yolo/object_detection_yolo.py
+++ test_yolo/object_detection_yolo.py
@@ -9,14 +9,13 @@ model = YOLO("yolov8n.pt")
 
 image_dir = "./Pictures"
 
+output_project = "./test_yolo/pictures_detected"
+
 for filename in os.listdir(image_dir):
     if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
         image_path = os.path.join(image_dir, filename)       
 
     # Predict on an image
-    results = model(image_path)
+    results = model(image_path, save=True, save_txt=False, save_conf=True, project=output_project, exist_ok=True)
 
-    # Plot the results
-    img = results[0].plot()
-    cv2.imshow("img", img)  # Display results
-    cv2.waitKey(0)
+print(f"Fertig! Ergebnisse in: {output_project}")
