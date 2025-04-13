e2d1132af5841cadeda84215901f5018b5d3e434
Sascha Zumstein <zumstein.sascha@bluewin.ch>
2025-04-13 11:33:00 +0200

    training pepper

--- training_pepper/object_detection_yolo.py
+++ training_pepper/object_detection_yolo.py
@@ -5,6 +5,6 @@ ultralytics.checks()
 model_path = "./training_pepper/models/model_V"
 image_dir = "./Test_Pictures"
 
-model = YOLO(model_path + "/weights/last.pt")
+model = YOLO(model_path + "/weights/best.pt")
 # Predict on an image
-results = model(image_dir, save=True, project=model_path)
+results = model(image_dir, save=True, project=model_path) 
