d097e18dfeee5324545959f8793906d4fc9f4f64
Sascha Zumstein <zumstein.sascha@bluewin.ch>
2025-04-13 13:28:26 +0200

    Filter brightness, person and blurry

--- CameraWebServer/showStream.py
+++ CameraWebServer/showStream.py
@@ -1,5 +1,12 @@
 import cv2
 import requests
+import ultralytics
+from ultralytics import YOLO
+ultralytics.checks()
+import numpy as np
+
+confidence_threshold = 0.3
+index = 0
 
 # ESP32-CAM Stream-URL
 URL = "http://192.168.204.60"
@@ -7,20 +14,50 @@ URL = "http://192.168.204.60"
 # Set resolution to UXGA (1600x1200)
 requests.get(URL + "/control?var=framesize&val=10")
 
+# Load YOLO
+model = YOLO("yolov8n.pt")
+
 # Start video-stream
 cap = cv2.VideoCapture(URL + ":81/stream")
+#cap = cv2.VideoCapture(0) # Test with laptop cam
 if not cap.isOpened():
     print("Error: Cannot open the stream!")
     exit()
+
+def is_bright_enough(image_gray, threshold=50):
+    brightness = np.mean(image_gray)
+    return brightness >= threshold
+
+def person_in_frame(results):
+    detected_classes = [results[0].names[int(box.cls)] for box in results[0].boxes] 
+    return ("person" in detected_classes)
+
+def is_not_blurry(image_gray, threshold=100.0):
+    laplacian_var = cv2.Laplacian(image_gray, cv2.CV_64F).var()
+    return laplacian_var > threshold
 
 while True:
     ret, frame = cap.read()
     if not ret:
         print("Error: No frame received!")
         break
-    
-    cv2.imshow("ESP32-CAM Stream", frame)
-    
+
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    if is_bright_enough(frame_gray):
+        # Predict on an image
+        results = model(frame, conf=confidence_threshold)
+        annotated_frame = results[0].plot()
+
+        # check if person is in image
+        if (not person_in_frame(results)) and is_not_blurry(frame_gray):
+            cv2.imwrite(f"./CameraWebServer/images/img_{index}.png", annotated_frame)
+            index += 1
+
+        cv2.imshow("YOLOv8", annotated_frame)
+
+    else:
+        cv2.destroyAllWindows()
+
     # escape with q
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
