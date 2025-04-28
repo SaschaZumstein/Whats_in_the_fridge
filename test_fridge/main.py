15d8ca8e68c5cfbac6311bfd3da3196dfcaeff24
Sascha Zumstein <153103542+SaschaZumstein@users.noreply.github.com>
2025-04-28 17:22:15 +0200

    Bildvorverarbeitung und Erkennung

--- test_fridge/main.py
+++ test_fridge/main.py
@@ -5,23 +5,24 @@ from ultralytics import YOLO
 ultralytics.checks()
 from collections import Counter
 import numpy as np
+import threading
+import queue
 
 confidence_threshold = 0.3
-index = 0
-file_path = "./test_fridge/images"
+image_path = "./test_fridge/images"
+yolov8_path = "yolov8n.pt"
+wanted_classes = [0, 39, 51]
+yolo_trained_path = "./training_pepper/models/model_V/weights/best.pt"
 
 # ESP32-CAM Stream-URL
-URL = "http://192.168.204.60"
-
-# Set resolution to UXGA (1600x1200)
-requests.get(URL + "/control?var=framesize&val=10")
+URL = "http://192.168.21.60"
 
 # Load YOLO
-model = YOLO("yolov8n.pt")
+model_yolo = YOLO(yolov8_path)
+model_trained = YOLO(yolo_trained_path)
 
-# Start video-stream
+# Start video-stream 
 cap = cv2.VideoCapture(URL + ":81/stream")
-#cap = cv2.VideoCapture(0) # Test with laptop cam
 if not cap.isOpened():
     print("Error: Cannot open the stream!")
     exit()
@@ -38,10 +39,17 @@ def is_not_blurry(image_gray, threshold=100.0):
     laplacian_var = cv2.Laplacian(image_gray, cv2.CV_64F).var()
     return laplacian_var > threshold
 
-def writeClassesToList(results, index):
-    detected_classes = [results[0].names[int(box.cls)] for box in results[0].boxes] 
-    class_counts = Counter(detected_classes)
-    with open(file_path + f"/img_{index}.txt", "w", encoding="utf-8") as f:
+def writeClassesToList(results_yolo, results_trained):
+    all_detected_classes = []
+
+    detected_classes_yolo = [results_yolo[0].names[int(box.cls)] for box in results_yolo[0].boxes] 
+    all_detected_classes.extend(detected_classes_yolo)
+
+    detected_classes_trained = [results_trained[0].names[int(box.cls)] for box in results_trained[0].boxes]
+    all_detected_classes.extend(detected_classes_trained)
+
+    class_counts = Counter(all_detected_classes)
+    with open(image_path + f"/list.txt", "w", encoding="utf-8") as f:
         for cls, count in class_counts.items():
             f.write(f"{count}x: {cls}" + "\n")
 
@@ -52,21 +60,28 @@ while True:
         break
 
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    if is_bright_enough(frame_gray):
-        # Predict on an image
-        results = model(frame, conf=confidence_threshold)
-        annotated_frame = results[0].plot()
+    if is_bright_enough(frame_gray) and is_not_blurry(frame_gray):
+        # Predict with yolo
+        results_yolo = model_yolo(frame, conf=confidence_threshold, classes=wanted_classes)
+        annotated_frame_yolo = results_yolo[0].plot()
 
-        # check if person is in image
-        if (not person_in_frame(results)) and is_not_blurry(frame_gray):
-            cv2.imwrite(file_path + f"/img_{index}.png", annotated_frame)
-            writeClassesToList(results, index)
-            index += 1
+        # check if no person is in image
+        if (not person_in_frame(results_yolo)):
+            # predict with self trained modell
+            results_trained = model_trained(frame, conf=confidence_threshold)
+            annotated_frame_trained = results_trained[0].plot()
 
-        cv2.imshow("YOLOv8", annotated_frame)
+            cv2.imwrite(image_path + "/img_yolo.png", annotated_frame_yolo)
+            cv2.imwrite(image_path + "/img_trained.png", annotated_frame_trained)
+            writeClassesToList(results_yolo, results_trained)
 
+        # For debug reason only
+            cv2.imshow("YOLO Trained", annotated_frame_trained)
+        cv2.imshow("YOLOv8", annotated_frame_yolo)
     else:
-        cv2.destroyAllWindows()
+        cv2.putText(frame, "Image too dark", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        cv2.putText(frame, "or too blurry", (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        cv2.imshow("YOLOv8", frame)
 
     # escape with q
     if cv2.waitKey(1) & 0xFF == ord('q'):
